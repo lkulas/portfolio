9b7017e8b0bd474dee16c895267195264e4a2b92
Laura Kulas <laura.kulas@gmail.com>
2018-11-21 12:29:41 -0700

    working nav, styling

--- script.js
+++ script.js
@@ -24,9 +24,61 @@ function watchClickLeft() {
   });
 };
 
+function watchClickName() {
+  $('#nav1').on('click', event => {
+    event.preventDefault();
+    currentView = 1;
+    for (let i=1; i <= pageCount; i++) {
+      $(`#div${i}`).prop('hidden', true);
+    };
+    $('#div1').prop('hidden', false);
+  });
+};
+
+function watchClickAbout() {
+  $('#nav2').on('click', event => {
+    event.preventDefault();
+    currentView = 2;
+    for (let i=1; i <= pageCount; i++) {
+      $(`#div${i}`).prop('hidden', true);
+    };
+    $('#div2').prop('hidden', false);
+  });
+};
+
+function watchClickWork() {
+  $('#nav3').on('click', event => {
+    event.preventDefault();
+    currentView = 3;
+    for (let i=1; i <= pageCount; i++) {
+      $(`#div${i}`).prop('hidden', true);
+    };
+    $('#div3').prop('hidden', false);
+  });
+};
+
+function watchClickContact() {
+  $('#nav4').on('click', event => {
+    event.preventDefault();
+    currentView = 6;
+    for (let i=1; i <= pageCount; i++) {
+      $(`#div${i}`).prop('hidden', true);
+    };
+    $('#div6').prop('hidden', false);
+  });
+};
+
+function handleNav() {
+  watchClickName();
+  watchClickAbout();
+  watchClickWork();
+  watchClickContact();
+};
+
 function handlePage() {
   watchClickRight();
   watchClickLeft();
+  handleNav();
 };
 
 $(handlePage());
